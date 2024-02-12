aa1a6211c3c78a9116be74b2e9eea0e45e56dd97
Tom Anthony Aguila <tom31aguila@gmail.com>
2024-02-12 11:16:53 -0800

    [mod] update setting function

--- src/controllers/Settings.ts
+++ src/controllers/Settings.ts
@@ -24,14 +24,24 @@ export const UpdateFare = async (
   res: express.Response
 ) => {
   try {
-    const newval = req.body.fare;
-    if (newval <= 0) {
+    const { newfare, newbal, newpesokm, newoperation } = req.body;
+    if (newfare <= 0 || newbal <= 0 || newpesokm <= 0) {
       return res.status(400).send("Cannot be a negative number");
     }
     const db = await getConnection();
     const data = await db
       .collection("settings")
-      .updateOne({ Title: "Settings" }, { $set: { Fare: newval } });
+      .updateOne(
+        { Title: "Settings" },
+        {
+          $set: {
+            Fare: newpesokm,
+            Balance: newbal,
+            MinimumFare: newfare,
+            Operations: newoperation,
+          },
+        }
+      );
     // console.log("res",articles)
     if (data.modifiedCount > 0) {
       // The update was successful
